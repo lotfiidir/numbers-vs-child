9c67716566b213e06fb97d86156e52911e00d2b3
Lotfi idir <lotfiidir1@gitlab.com>
2018-01-16 18:31:23 +0100

    create ListNumber componenet and display numbers in route /list

---
+++ src/components/ListNumber.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="n in 10" @click="store.currentNumber = n">
+      {{ n }}
+    </div>
+    <pre>{{ store }}</pre>
+  </div>
+</template>
+
+<script>
+    import Number from "./Number";
+    import store from "../store";
+
+    export default {
+      components: {Number},
+      name: "list-number",
+    data() {
+      return {
+        store: store
+      }
+    }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Number.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{ store.number }}</p>
+  </div>
+</template>
+
+<script>
+  import store from '../store'
+
+  export default {
+    name: "number",
+    data() {
+      return {
+        store: store
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
