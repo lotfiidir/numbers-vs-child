16dedc4475c4f0ff3f7da9a924eb486c3afb5b60
TRAORE <sirimant@yahoo.fr>
2018-01-29 21:00:22 +0100

    Refactoring store, vue evaluation

--- src/components/evaluationComponent.vue
+++ src/components/evaluationComponent.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
-    <div @click="store.evaluation.step = 10">jump</div>
-    <div class="progress-bar" v-bind:style="{ width: 10 * store.evaluation.step + '%' }">
+    <div class="progress-bar" v-bind:style="{ width: 10 * store.game.getCurrentPartie().step + '%' }">
       <p>
-        {{store.evaluation.step}}
+        {{store.game.getCurrentPartie().step}}
       </p>
     </div>
     <div class="operation">
       <h2>{{ $route.params.id }}</h2>
       <span class="multiplicator"></span>
-      <h2>{{store.evaluation.operandeB}}</h2>
+      <h2>{{store.game.getCurrentPartie().operandeB}}</h2>
     </div>
     <div class="equal"></div>
     <div class="response number">
-      <h2 v-for="response in store.evaluation.responses" @click="next(response, $event)">{{response}}</h2>
+      <h2 v-for="response in store.game.getCurrentPartie().responses" @click="next(response, $event)">{{response}}</h2>
     </div>
     <div class="minuetrie">{{datenow}}</div>
     <pre>{{ store }}</pre>
@@ -46,24 +45,27 @@
         }, 30)
       },
       init() {
+        /*
         var serie = {
-          "serie": this.datenow, "operations": []
+          "serie": new Date(), "operations": []
         };
-        var child = store.evaluation.getCurrentChild();
+        var child = store.game.getCurrentChild();
         child.evaluations.push(serie);
-        console.log("ITEM ",child);
-        store.evaluation.updateChildren(child);
-        store.evaluation.initialization(this.$route.params.id);
+        //console.log("ITEM ",child);
+        store.game.updateChildren(child);*/
+        //store.game.initialization(this.$route.params.id);
+        store.game.init("game", this.$route.params.id)
       },
       next(response, $event) {
-        const eval_store = store.evaluation;
+        const eval_store = store.game;
+        const item_part = store.game.getCurrentPartie();
         /*let titi = new Children("Mami", [], []);
         var titi = {
           "pseudo": "Mami", "apprentissages":[], "evaluations":[]
         };*/
         //console.log("CREATE ", eval_store.createChild(titi));
         //console.log("CREATE ", eval_store.setCurrentChild("Mami"));
-        console.log("GET ITEM", eval_store.getCurrentChild());
+        //console.log("GET ITEM", eval_store.getCurrentChild());
         const answer = {};
         answer.step = eval_store.step;
         answer.operandeA = eval_store.operandeA;
@@ -74,11 +76,12 @@
         if (eval_store.result != response) {
           answer.echec = true;
         }
-        eval_store.stories.push(answer);
+        //eval_store.stories.push(answer);
 
         setTimeout(() => {
-          if (eval_store.step > 9) {
-            eval_store.done = true;
+          if (item_part.step > 9) {
+            item_part.done = true;
+            eval_store.setCurrentPartie(item_part);
             this.$router.push({name: 'recap', params: {name: 'recap'}});
             return;
           }
