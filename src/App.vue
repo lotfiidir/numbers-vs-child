ab8a3fd51bedab81ef1e85789017aee8a97203fb
Lotfi idir <lotfiidir1@gitlab.com>
2018-01-29 17:51:53 +0100

    add ajouter link

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
     <div id="header">
+      <div><a href="#" @click="modal = !modal">Ajouter un enfant</a></div>
       <div id="logo">
         <router-link to="/">
           <h2>Numbers VS Child</h2>
         </router-link>
       </div>
+    </div>
+    <div class="modal-addchild" v-if="modal">
+      <h2>Ajout d'un enfant</h2>
+      <input type="text" v-model="enfant" />
+      {{enfant}}
     </div>
     <router-view/>
   </div>
@@ -13,7 +19,15 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+data(){
+  return {
+    modal: false,
+    enfant: '',
+    }
+  },
+  methods: {
+  }
 }
 </script>
 
